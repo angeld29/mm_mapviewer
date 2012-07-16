3b1b397201b80fe0b9791e64472ec8f8ef416e7b
angeld <angeldeath29@gmail.com>
2012-07-16 15:15:42 +0400

    GPL

--- mm_mapviewer/src/mathlib.cpp
+++ mm_mapviewer/src/mathlib.cpp
@@ -1,3 +1,22 @@
+/*
+
+Copyright (C) 2009 Angel (angel.d.death@gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the included (GNU.txt) GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 #include "stdafx.h"
 
 void AngleVectors ( const vec3_t& angles, vec3_t* forward, vec3_t* right, vec3_t* up)
